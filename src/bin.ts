a526ed685e137fcec29d6ef4cb08c759ce5b4e50
Hoishin <hoishinxii@gmail.com>
2019-05-24 01:36:16 +0900

    add "start" script

--- src/bin.ts
+++ src/bin.ts
@@ -1,5 +1,6 @@
 import yargs from 'yargs';
 import {postinstall} from './postinstall';
+import {start} from './start';
 
 export const enum Command {
 	Postinstall = 'postinstall',
@@ -9,6 +10,7 @@ export const enum Command {
 const {argv} = yargs
 	.demandCommand(1)
 	.command(Command.Postinstall, 'Set this command to npm `postinstall` hook')
+	.command(Command.Start, 'Start NodeCG')
 	.strict();
 
 const [command] = argv._;
@@ -17,7 +19,11 @@ switch (command) {
 	case Command.Postinstall:
 		postinstall();
 		break;
+	case Command.Start:
+		start();
+		break;
 	default:
 		console.error('Unexpected command:', command);
+		process.exitCode = 1;
 		break;
 }
---
+++ src/start.ts
@@ -0,0 +1,3 @@
+export const start = (): void => {
+	require('nodecg');
+};
